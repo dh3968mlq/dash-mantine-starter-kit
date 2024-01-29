568d7ad6c5f85220b2c66f1be0f3c60f17653b1d
David Harris <dharris3968@hotmail.co.uk>
2024-01-29 21:57:16 +0000

    Move applyaout.py to lib

--- app.py
+++ app.py
@@ -5,14 +5,14 @@
 '''
 from dash import Dash, page_registry
 import gunicorn # Necessary for Heroku?
-import applayout
+from lib.applayout import get_layout
 
 app = Dash(__name__, use_pages=True)
 app._favicon = "favicon.png"
 server = app.server  # Necessary for Heroku?
 
 app.title = "Dash Mantine starter kit"
-app.layout = applayout.get_layout(page_registry.values())
+app.layout = get_layout(page_registry.values())
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8050)
