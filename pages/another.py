326f8ff25e7b81a3468ddc8b820c789dde22709a
David Harris <dharris3968@hotmail.co.uk>
2024-01-30 09:28:50 +0000

    add lorem.py

--- pages/another.py
+++ pages/another.py
@@ -1,14 +1,8 @@
 import dash
 from dash import html
+from lib.lorem import lorem
 
 dash.register_page(__name__, title='Another Page')
-
-lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " \
-        "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " \
-        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut " \
-        "aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in " \
-        "voluptate velit esse cillum dolore eu fugiat nulla pariatur. " \
-        "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 layout = html.Div([
     html.H1('This is another page'),
