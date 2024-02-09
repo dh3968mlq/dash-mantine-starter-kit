75f57902b34a02ceab92b01066ee9942d99e3e4a
David Harris <dharris3968@hotmail.co.uk>
2024-02-09 15:23:58 +0000

    Add Heroku files

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ from lib.applayout import get_layout
 from lib import callback_close_drawer    # The import defines the callback, no need to reference it
 from lib import callback_open_drawer
 
-app = Dash(__name__, use_pages=True)  # https://dash.plotly.com/urls
-app._favicon = "favicon.png"       # app.title must be set page by page
+app = Dash(__name__, use_pages=True)    # A multi-page app: https://dash.plotly.com/urls
+app._favicon = "favicon.png"            # app.title must be set page by page
 server = app.server  # Necessary for Heroku?
 
 app.layout = get_layout(page_registry.values())
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=8050)
+    app.run(debug=False, host='0.0.0.0', port=8050)  # debug=True seems a bit unreliable with Mantine components
